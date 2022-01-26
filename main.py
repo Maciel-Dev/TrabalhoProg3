bf6e7e8af4ee5dae0a70a5e22b79e9a86051002c
João Vitor Maciel Vianna <jv.vianna@hotmail.com>
2022-01-25 23:44:33 -0300

    ''

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ def main():
     #Cria uma variável com o arquivo para leitura
     archive = open("entrada.bin", 'rb')
     dictionario = pickle.load(archive)
-    print(dictionario)
+    lista = pickle.load(archive)
+
+    #Chama função de cálculo de notas
+    listaNotas = calculoNotaTotal(dictionario, lista)
+    #Debug listaNotas
+    print(f'\n\nAqui:{listaNotas}')
+
+#Calculo de notas e retorna lista
+def calculoNotaTotal(dictionario, lista):
+    listaAlunos = []
+    for aluno in lista:
+        matricula, nota1, nota2, nota3, nota4, tempo = aluno
+        listaAlunos.append([matricula, nota1+nota2+nota3+nota4, tempo])
+
+    return listaAlunos
+
+def ordenar():
+    pass
+
+
+
 
 #Chama função principal
 main()
